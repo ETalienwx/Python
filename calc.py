cfdcadaeae0cd9ea186013178f799c20d3436835
wangxuan <wangxuan0809@126.com>
2019-11-20 22:43:59 +0800

    calc

---
+++ calc.py
@@ -0,0 +1,24 @@
+__author__ = 'asus'
+"""
+这个文档提供了加减乘除的基本功能
+"""
+def add(x,y):
+    """
+    完成两个对象的相加
+    """
+    return x+y
+def sub(x,y):
+    """
+    完成两个对象的相减
+    """
+    return x-y
+def mul(x,y):
+    """
+    完成两个对象的相乘
+    """
+    return x*y
+def divide(x,y):
+    """
+    完成两个对象的相除
+    """
+    return x/y
